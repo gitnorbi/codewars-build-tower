2765da7c63263a04c659b2ab2d5ae3e83850d283
norbigith <dulnorbert@gmail.com>
2021-11-19 18:06:36 +0100

    Itt works well

---
+++ index.js
@@ -0,0 +1,25 @@
+function towerBuilder(nFloors) {
+    let building=[];
+    let stars='*';
+    let numberOfCharsInLatestRow=nFloors*2-1;
+    let starsWithSpace;
+    howMuchSpaceNeededToOneSide=(numberOfCharsInLatestRow-stars.length)/2;
+    starsWithSpace=" ".repeat(howMuchSpaceNeededToOneSide)+stars+" ".repeat(howMuchSpaceNeededToOneSide);
+    building.push(starsWithSpace);
+    document.querySelector("#result").innerHTML+=starsWithSpace+"<br>";
+    for(let i=2;i<=nFloors;i++){
+        let howMuchSpaceNeededToOneSide;
+        
+        stars+='**';
+        howMuchSpaceNeededToOneSide=(numberOfCharsInLatestRow-stars.length)/2;
+        starsWithSpace=" ".repeat(howMuchSpaceNeededToOneSide)+stars+" ".repeat(howMuchSpaceNeededToOneSide);
+        building.push(starsWithSpace);
+        document.querySelector("#result").innerHTML+=starsWithSpace+"<br>";
+        console.log(howMuchSpaceNeededToOneSide);
+    }
+
+    document.querySelector("#theResultSorFloor").textContent+=` for ${nFloors} floors:`;
+    return building;
+  }
+
+  towerBuilder(20);
